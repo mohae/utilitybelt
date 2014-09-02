6191d0c3157ee5c83174728e4910fe9bcb60710c
Joel Scoble <joel.scoble@outlook.com>
2014-09-02 16:38:00 -0500

    added a new function

--- utils.go
+++ utils.go
@@ -1,5 +1,11 @@
 package utilitybelt
 
+import (
+	"errors"
+	_ "bytes"
+	"strings"
+)
+
 // BoolToString converts a bool to its corresponding string value, in lowercase
 func BoolToString(b bool) string {
 	if b {
@@ -10,12 +16,12 @@ func BoolToString(b bool) string {
 }
 
 // BoolToByte converts a bool to its corresponding byte value, t or f
-func BoolToLetter(b bool) byte {
+func BoolToLetter(b bool) string {
         if b {
-                return byte("t")
+                return "t"
         }
 
-        return byte("f")
+        return "f"
 }
 
 // StringIsBool checks the string to see if it represeents a boolean value.
@@ -27,4 +33,19 @@ func StringIsBool(s string) bool {
 			return true
 	}
 	return false
+}
+
+// StringToBool checks the string to see if it represents a boolean value. If 
+// it does, its boolean version is returned. Otherwise an error.
+// Only true and false versions are supported. 
+// TODO: should "0", "1", "on", "off", etc be supported here or in other funcs
+func StringToBool(s string) (bool, error) {
+	switch strings.ToLower(s) {
+		case "t", "true":
+			return true, nil
+		 case "f", "false":
+			return false, nil
+	}
+
+	return false, errors.New(s + " is not a supported boolean value")
 }
